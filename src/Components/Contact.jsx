3d30f1014fd6fac5129f27b9d0907aa64ebdbd39
Lakshan Jayalath <lakshanjayalath171@gmail.com>
2026-02-20 18:32:43 +0530

    added new features to window

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const WindowDesc = () => {
+    return (
+        <div className="w-20 h-20 border-none rounded-3xl">
+            
+        </div>
+    )
+}
+export default WindowDesc
