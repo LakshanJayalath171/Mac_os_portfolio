b6ba288c55ae1c603b61ebfd56b3d2a5d08a36c8
Lakshan Jayalath <lakshanjayalath171@gmail.com>
2026-02-26 12:48:52 +0530

    All UI created and all functions implemented in contact window

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -15,14 +15,35 @@ import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 const Contact = () => {
 
     const [pannel,setpannel] = useState(1);
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+        formData.append("access_key", "96614379-9470-4cf6-aeff-e09dcccb9291");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            setResult("Error");
+        }
+    };
 
 
-    console.log(pannel)
     return (
         <div className='mac flex relative h-[400px] w-[700px] rounded-[8px]'>
             {/*first division */}
             <div className="flex-1 rounded-2xl">
 
+                {/*Controls in upper section*/}
                 <div className='p-4 flex items-center '>
                     <div className="w-3 h-3 rounded-full bg-red-600 ml-2"></div>
                     <div className="w-3 h-3 rounded-full bg-amber-600 ml-2"></div>
@@ -36,10 +57,11 @@ const Contact = () => {
                         <input className="w-60 ml-3 relative mr-3 rounded-[5px] h-6 border-none outline-none mac placeholder:text-sm py-2" />
                     </div>
 
+                    {/*Mapped items from the array */}
                     <div className="text-left mt-5">
 
                         {social.map((items,i)=>(
-                            <div className="p-2 pt-1 pb-0 flex items-start gap-2 cursor-pointer" key={items.id}>
+                            <div onClick={()=>setpannel(items.id)} className="p-2 pt-1 pb-0 flex items-start gap-2 cursor-pointer" key={items.id}>
                                 <img className="w-5 h-5" src={items.image}/><p className="text-shadow-black font-semibold text-[15px] poppins-bold">{items.name}</p>
 
                             </div>
@@ -87,19 +109,36 @@ const Contact = () => {
 
 
                     {social.map((items,index)=>(
-                        <div>
+                        <div className={pannel==items.id?"":"hidden"}>
                             <h2 className="poppins-extrabold text-2xl">{items.name}</h2>
-                            <div className="mac rounded-2xl m-2 flex ">
-                                <div>
+                            <div className="mac rounded-2xl m-2 flex p-3">
+                                <div className="flex-1">
                                     <img src={items.image} className='w-25 h-25'/>
                                 </div>
 
-                                <div className="p-3">
+                                <div className={`p-3 ${pannel==5?"hidden":""}`}>
                                     <h2 className="text-gray-800 poppins-bold text-[18px]">Name</h2>
-                                    <button className="px-7 py-2 rounded-full bg-blue-600"><a href={items.url}>Send Message</a> <ArrowDropUpIcon/></button>
+                                    <button className="px-7 py-2 rounded-full bg-blue-600"><a href={items.url}>Explore</a> <ArrowDropUpIcon/></button>
+                                </div>
+
+                                {/*Gmail section */}
+                                <div className={`${pannel==5?"":"hidden"} w-50` }>
+                                    <form onSubmit={onSubmit} className="flex flex-col">
+                                        <input className="mac-input placeholder:poppins-bold placeholder:font-bold" placeholder="Your Name" type="text" name="name" required/>
+                                        <input className="mac-input placeholder:font-bold" type="email" placeholder="Your Email" name="email" required/>
+                                        <textarea className="mac-input placeholder:font-bold" placeholder="Message" name="message" required></textarea>
+                                        <button className="bg-blue-600 m-2 p-2 rounded-full" type="submit">Submit Form <ArrowDropUpIcon/> </button>
+                                        <span className="poppins-bold text-gray-800">{result}</span>
+                                    </form>
                                 </div>
                             </div>
+
+
+
+
                         </div>
+
+
                     ))}
                 </div>
 
